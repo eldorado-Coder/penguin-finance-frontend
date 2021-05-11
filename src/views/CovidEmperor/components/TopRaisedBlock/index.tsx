579d51e1d057f560162e58be24d005997159444b
evercreativedev <evercreativedev@gmail.com>
2021-05-12 04:38:09 +0800

    feat: handled responsiveness of banners and boxes

--- src/views/CovidEmperor/components/TopRaisedBlock/index.tsx
+++ src/views/CovidEmperor/components/TopRaisedBlock/index.tsx
@@ -3,34 +3,19 @@ import styled from 'styled-components'
 import { useWeb3React } from '@web3-react/core'
 import { Text, Flex } from 'penguinfinance-uikit2'
 import BigNumber from 'bignumber.js'
-import UnlockButton from 'components/UnlockButton'
 import { useEmperor, useDonations } from 'state/hooks'
 import { getBalanceNumber } from 'utils/formatBalance'
 import SvgIcon from 'components/SvgIcon'
 import { getPenguinColor } from '../utils'
-
-const CardBlock = styled.div<{ account: string }>`
-  display: ${props => props.account && 'flex'};
-  flex-direction: column;
-  align-items: center;
-`
-
-const CardBlockHeader = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  z-index: 1;
-  padding: 16px;
-  width: 100%;
-`
+import { UnlockButton as Button, Caption, SubTitle, CardBlockHeader, CardBlock } from '../UI'
 
 const TitleBgWrapper = styled.div<{ color: string }>`
   z-index: -1;
   width: 100%;
   text-align: center;
-  transform: scale(1.5);
+  
   position: absolute;
-  margin-top: 30%;
+  margin-top: 15%;
 
   svg {
     #Banner-Avatar {
@@ -39,18 +24,49 @@ const TitleBgWrapper = styled.div<{ color: string }>`
       }
     }
   }
+
+  transform: scale(1.8);
+  @media (min-width: 640px) {
+    transform: scale(1.5);
+  }
+  @media (min-width: 1200px) and (max-height: 800px) {
+    transform: scale(1.8);
+  }
 `
 
 const CardBlockContent = styled.div<{ account: string }>`
   background: ${(props) => props.theme.card.background};
-  border-radius: 16px;
-  position: relative;
-  padding: 24px;
-  padding-bottom: 16px;
+  border-radius: 8px;
   position: relative;
   text-align: center;
-  min-width: 240px;
-  margin-top: ${props => props.account ? '57%' : '55%'};
+  
+  margin-top: 25%;
+  min-width: 100px;
+  padding: 24px 8px 8px;
+  @media (min-width: 640px) {
+    width: 100%;
+    margin-top: 34%;
+    padding: 32px 16px 12px;
+  }
+  @media (min-width: 768px) {
+    width: 100%;
+    padding: 40px 20px 16px;
+    margin-top: 38%;
+  }
+  @media (min-width: 1200px) {
+    width: 100%;
+    border-radius: 16px;
+    padding: 48px 24px 16px;
+    margin-top: 38%;
+  }
+  @media (min-width: 1450px) {
+    min-width: 240px;
+    padding: 40px 24px 16px;
+    margin-top: 42%;
+  }
+  @media (min-width: 1200px) and (max-height: 800px) {
+    padding-top: 24px;
+  }
 `
 
 const WalletContainer = styled.div`
@@ -63,6 +79,12 @@ const EmperorInfoContainer = styled.div`
   z-index: 10;
 `
 
+const UnlockButton = styled(Button)`
+  @media (min-width: 1200px) {
+    margin-top: -8px;
+  }
+`;
+
 const TopRaisedBlock: React.FC = () => {
   const { account } = useWeb3React()
   const { currentEmperor } = useEmperor();
@@ -74,7 +96,7 @@ const TopRaisedBlock: React.FC = () => {
   const pefiBurnt = getBalanceNumber(new BigNumber(donations.totalPefiRaised * 0.25));
 
   return (  
-    <CardBlock account={account}>
+    <CardBlock>
       <CardBlockHeader>
         <TitleBgWrapper color={headerColor}>
           <SvgIcon
@@ -93,16 +115,16 @@ const TopRaisedBlock: React.FC = () => {
         {account && donations && (
           <EmperorInfoContainer>
             <Flex justifyContent='space-between'>
-              <Text bold color='secondary' fontSize="18px">AVAX</Text>
-              <Text bold color="primaryBright" fontSize="18px">{totalAvaxRaised.toFixed((!totalAvaxRaised || totalAvaxRaised > 1) ? 0 : 3)}</Text>
+              <SubTitle bold color='secondary'>AVAX</SubTitle>
+              <SubTitle bold color="primaryBright">{totalAvaxRaised.toFixed((!totalAvaxRaised || totalAvaxRaised > 1) ? 0 : 3)}</SubTitle>
             </Flex>
             <Flex justifyContent='space-between'>
-              <Text bold color='secondary' fontSize="18px">PEFI</Text>
-              <Text bold color="primaryBright" fontSize="18px">{totalPefiRaised.toFixed((!totalPefiRaised || totalPefiRaised > 1) ? 0 : 3)}</Text>
+              <SubTitle bold color='secondary'>PEFI</SubTitle>
+              <SubTitle bold color="primaryBright">{totalPefiRaised.toFixed((!totalPefiRaised || totalPefiRaised > 1) ? 0 : 3)}</SubTitle>
             </Flex>
             <Flex justifyContent='space-between' mt='8px'>
-              <Text bold color='secondary' fontSize="14px">PEFI Burnt</Text>
-              <Text bold color="primaryBright" fontSize="14px">{pefiBurnt.toFixed((!pefiBurnt || pefiBurnt > 1) ? 0 : 3)}</Text>
+              <Caption bold color='secondary'>PEFI Burnt</Caption>
+              <Caption bold color="primaryBright">{pefiBurnt.toFixed((!pefiBurnt || pefiBurnt > 1) ? 0 : 3)}</Caption>
             </Flex>
           </EmperorInfoContainer>
         )}
