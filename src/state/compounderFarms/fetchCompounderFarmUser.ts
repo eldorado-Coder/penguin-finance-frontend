c1c3b1f4876c2758f241c09af98053b5dacf2d5e
daiki saito <daiki.saito92@gmail.com>
2021-06-09 03:13:09 +0900

    fix deposit/withdraw issue of pangolin pools

--- src/state/compounderFarms/fetchCompounderFarmUser.ts
+++ src/state/compounderFarms/fetchCompounderFarmUser.ts
@@ -3,19 +3,19 @@ import erc20ABI from 'config/abi/erc20.json'
 import multicall from 'utils/multicall'
 import farmsConfig from 'config/constants/compounderFarms'
 import { getAddress } from 'utils/addressHelpers'
-import getFarmMasterChefAbi from 'utils/getFarmMasterChefAbi';
-import getFarmMasterChefAddress from 'utils/getFarmMasterChefAddress';
-import getStrategyAddress from 'utils/getStrategyAddress';
-import getStrategyAbi from 'utils/getStrategyAbi';
+import getFarmMasterChefAbi from 'utils/getFarmMasterChefAbi'
+import getFarmMasterChefAddress from 'utils/getFarmMasterChefAddress'
+import getStrategyAddress from 'utils/getStrategyAddress'
+import getStrategyAbi from 'utils/getStrategyAbi'
 
 export const fetchCompounderFarmUserAllowances = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     const lpContractAddress = getAddress(farm.lpAddresses)
-    const strategyContractAddress = getStrategyAddress(farm.lpSymbol, farm.type);
+    const strategyContractAddress = getStrategyAddress(farm.lpSymbol, farm.type)
     return { address: lpContractAddress, name: 'allowance', params: [account, strategyContractAddress] }
   })
 
-  const rawLpAllowances = await multicall(erc20ABI, calls);
+  const rawLpAllowances = await multicall(erc20ABI, calls)
   const parsedLpAllowances = rawLpAllowances.map((lpBalance) => {
     return new BigNumber(lpBalance).toJSON()
   })
@@ -42,7 +42,7 @@ export const fetchCompounderFarmUserTokenBalances = async (account: string) => {
 }
 
 export const fetchCompounderFarmUserStakedBalances = async (account: string) => {
-  const results = [];
+  const results = []
   for (let i = 0; i < farmsConfig.length; i++) {
     // const call = [{
     //   address: getFarmMasterChefAddress(farmsConfig[i].type),
@@ -53,18 +53,20 @@ export const fetchCompounderFarmUserStakedBalances = async (account: string) => 
     // const masterChefABI = getFarmMasterChefAbi(farmsConfig[i].type);
     // const farmRes = multicall(masterChefABI, call);
     // results.push(farmRes);
-    const call = [{
-      address: getStrategyAddress(farmsConfig[i].lpSymbol, farmsConfig[i].type),
-      name: 'balanceOf',
-      params: [account]
-    }]
+    const call = [
+      {
+        address: getStrategyAddress(farmsConfig[i].lpSymbol, farmsConfig[i].type),
+        name: 'balanceOf',
+        params: [account],
+      },
+    ]
 
-    const masterChefABI = getStrategyAbi(farmsConfig[i].lpSymbol, farmsConfig[i].type);
-    const farmRes = multicall(masterChefABI, call);
-    results.push(farmRes);
+    const masterChefABI = getStrategyAbi(farmsConfig[i].lpSymbol, farmsConfig[i].type)
+    const farmRes = multicall(masterChefABI, call)
+    results.push(farmRes)
   }
 
-  const rawStakedBalances = await Promise.all(results);
+  const rawStakedBalances = await Promise.all(results)
   const parsedStakedBalances = rawStakedBalances.map((stakedBalance) => {
     return new BigNumber(stakedBalance[0][0]._hex).toJSON()
   })
@@ -72,20 +74,34 @@ export const fetchCompounderFarmUserStakedBalances = async (account: string) => 
 }
 
 export const fetchCompounderFarmUserEarnings = async (account: string) => {
-  const results = [];
+  const results = []
   for (let i = 0; i < farmsConfig.length; i++) {
-    const call = [{
-      address: getFarmMasterChefAddress(farmsConfig[i].type),
-      name: farmsConfig[i].name,
-      params: [farmsConfig[i].pid, account],
-    }];
+    const masterChefAddress =
+      farmsConfig[i].type === 'Pangolin' ? farmsConfig[i].stakingAddress : getFarmMasterChefAddress(farmsConfig[i].type)
 
-    const masterChefABI = getFarmMasterChefAbi(farmsConfig[i].type);
-    const farmRes = multicall(masterChefABI, call);
-    results.push(farmRes);
+    const call =
+      farmsConfig[i].type === 'Pangolin'
+        ? [
+            {
+              address: masterChefAddress,
+              name: farmsConfig[i].name,
+              params: [account],
+            },
+          ]
+        : [
+            {
+              address: masterChefAddress,
+              name: farmsConfig[i].name,
+              params: [farmsConfig[i].pid, account],
+            },
+          ]
+
+    const masterChefABI = getFarmMasterChefAbi(farmsConfig[i].type)
+    const farmRes = multicall(masterChefABI, call)
+    results.push(farmRes)
   }
 
-  const rawEarnings = await Promise.all(results);
+  const rawEarnings = await Promise.all(results)
 
   const parsedEarnings = rawEarnings.map((earnings) => {
     return new BigNumber(earnings[0]).toJSON()
--- src/utils/getFarmMasterChefAbi.ts
+++ src/utils/getFarmMasterChefAbi.ts
@@ -1,7 +1,7 @@
 import masterchefABI from 'config/abi/masterchef.json'
 import gondolaMasterchefABI from 'config/abi/gondolaMasterChef.json'
 import lydiaMasterchefABI from 'config/abi/lydiaMasterChef.json'
-// import pangolinManagerABI from 'config/abi/pangolinManager.json'
+import pangolinManagerABI from 'config/abi/pangolinManager.json'
 import pangolinStakingABI from 'config/abi/pangolinStaking.json'
 
 const getFarmMasterChefAbi = (farmType: string) => {
@@ -14,6 +14,8 @@ const getFarmMasterChefAbi = (farmType: string) => {
       return gondolaMasterchefABI
     case 'Pangolin':
       return pangolinStakingABI
+    case 'Pangolin-Manager':
+      return pangolinManagerABI
     default:
       return masterchefABI
   }
