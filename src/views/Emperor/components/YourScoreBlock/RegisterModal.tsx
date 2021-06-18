2d921062c3cc3921421a4ecf937f8a6afe75ec47
daiki saito <daiki.saito92@gmail.com>
2021-06-18 19:04:22 +0900

    update styles of emperor modals

--- src/views/Emperor/components/YourScoreBlock/RegisterModal.tsx
+++ src/views/Emperor/components/YourScoreBlock/RegisterModal.tsx
@@ -1,15 +1,21 @@
 import React, { useCallback, useState } from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import { Button, Modal, Flex } from 'penguinfinance-uikit2'
 import ModalActions from 'components/ModalActions'
 import FieldInput from 'components/FieldInput'
 import useI18n from 'hooks/useI18n'
-import PenguinSelect from '../UI/PenguinSelect/PenguinSelect';
-import { penguinImages } from '../utils';
+import PenguinSelect from '../UI/PenguinSelect/PenguinSelect'
+import { penguinImages } from '../utils'
+
+// content
+const ModalContent = styled.div`
+  border-top: ${({ theme }) => (theme.isDark ? '1px solid #26183f' : '1px solid #e9eaeb')};
+  padding: 24px 24px 16px;
+`
 
 const PenguinSelectContainer = styled.div`
   padding: 8px 24px 0;
-`;
+`
 
 interface RegisterModalProps {
   onConfirm: (nickName: string, color: string, style: string) => void
@@ -39,48 +45,51 @@ const RegisterModal: React.FC<RegisterModalProps> = ({ onConfirm, onDismiss }) =
   )
 
   return (
-    <Modal title={TranslateString(1068, 'Register your penguin')} onDismiss={onDismiss}>
-      <FieldInput
-        value={nickName}
-        placeholder="Cubiz"
-        onChange={onChangeNickName}
-        inputTitle={TranslateString(1070, 'Nickname')}
-      />
-      <FieldInput
-        value={color}
-        placeholder="Red"
-        onChange={onChangeColor}
-        inputTitle={TranslateString(1070, 'Color')}
-      />
-      <PenguinSelectContainer>
-        <PenguinSelect
-          value={style}
-          placeholder='Select your penguin'
-          options={penguinImages.map(penguin => ({
-            label: penguin.name, value: penguin.id
-          }))}
-          onChange={setStyle}
+    <Modal title={TranslateString(1068, 'Register your penguin')} bodyPadding="0px" onDismiss={onDismiss}>
+      <ModalContent>
+        <FieldInput
+          value={nickName}
+          placeholder="Cubiz"
+          onChange={onChangeNickName}
+          inputTitle={TranslateString(1070, 'Nickname')}
         />
-      </PenguinSelectContainer>
-      <ModalActions>
-        <Flex justifyContent='space-between' pl='24px' pr='24px'>
-          <Button variant="primary" onClick={onDismiss} scale="md">
-            {TranslateString(462, 'Cancel')}
-          </Button>
-          <Button
-            scale="md"
-            disabled={pendingTx || nickName.length === 0 || color.length === 0 || style.length === 0}
-            onClick={async () => {
-              setPendingTx(true)
-              await onConfirm(nickName, color, style)
-              setPendingTx(false)
-              onDismiss()
-            }}
-          >
-            {pendingTx ? TranslateString(488, 'Pending Confirmation') : TranslateString(464, 'Confirm')}
-          </Button>
-        </Flex>
-      </ModalActions>
+        <FieldInput
+          value={color}
+          placeholder="Red"
+          onChange={onChangeColor}
+          inputTitle={TranslateString(1070, 'Color')}
+        />
+        <PenguinSelectContainer>
+          <PenguinSelect
+            value={style}
+            placeholder="Select your penguin"
+            options={penguinImages.map((penguin) => ({
+              label: penguin.name,
+              value: penguin.id,
+            }))}
+            onChange={setStyle}
+          />
+        </PenguinSelectContainer>
+        <ModalActions>
+          <Flex justifyContent="space-between" pl="24px" pr="24px">
+            <Button variant="primary" onClick={onDismiss} scale="md">
+              {TranslateString(462, 'Cancel')}
+            </Button>
+            <Button
+              scale="md"
+              disabled={pendingTx || nickName.length === 0 || color.length === 0 || style.length === 0}
+              onClick={async () => {
+                setPendingTx(true)
+                await onConfirm(nickName, color, style)
+                setPendingTx(false)
+                onDismiss()
+              }}
+            >
+              {pendingTx ? TranslateString(488, 'Pending Confirmation') : TranslateString(464, 'Confirm')}
+            </Button>
+          </Flex>
+        </ModalActions>
+      </ModalContent>
     </Modal>
   )
 }
