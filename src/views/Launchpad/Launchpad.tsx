56a6f74b44b6c4049531e7b8c8006d63abb969be
daiki saito <daiki.saito92@gmail.com>
2021-07-28 18:37:44 +0900

    change banners

--- src/views/Launchpad/Launchpad.tsx
+++ src/views/Launchpad/Launchpad.tsx
@@ -31,8 +31,8 @@ const Launchpad: React.FC = () => {
         <BannerImage
           src={
             isDark
-              ? `${process.env.PUBLIC_URL}/images/launchpad/banners/launchpad_banner_dark.png`
-              : `${process.env.PUBLIC_URL}/images/launchpad/banners/launchpad_banner_light.png`
+              ? `${process.env.PUBLIC_URL}/images/launchpad/banners/launchpad_banner_dark.gif`
+              : `${process.env.PUBLIC_URL}/images/launchpad/banners/launchpad_banner_light.gif`
           }
           alt="launchpad banner"
         />
