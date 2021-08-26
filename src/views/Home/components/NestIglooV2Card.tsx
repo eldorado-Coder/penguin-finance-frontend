baf9255ee4535acf84469d5d79739e76485625f5
daiki saito <daiki.saito92@gmail.com>
2021-08-26 11:08:17 +0900

    change background of iglooV2 on home page

--- src/views/Home/components/NestIglooV2Card.tsx
+++ src/views/Home/components/NestIglooV2Card.tsx
@@ -13,12 +13,18 @@ const StyledNestIglooCardBg = styled.div`
   background-size: cover;
   background-position: center center;
   background-image: url('/images/home/ipefi-introducing.png');
+  cursor: pointer;
 `
 
 const NestIglooV2Card = () => {
+  const onClickNestIglooV2Card = () => {
+    const iglooV2Url = 'https://penguin-finance.medium.com/introducing-ipefi-the-nest-evolution-d002f8548276'
+    window.open(iglooV2Url, '_blank')
+  }
+
   return (
     <StyledNestIglooCard>
-      <StyledNestIglooCardBg />
+      <StyledNestIglooCardBg onClick={onClickNestIglooV2Card} />
     </StyledNestIglooCard>
   )
 }
