76341a907f10eb1178a63c669e8d675d6ac4de9b
evercreativedev <evercreativedev@gmail.com>
2021-09-02 22:13:03 +0800

    feat: updated lgloo v2 page

--- src/views/FarmsV2/components/FarmTable/Actions/ActionPanel.tsx
+++ src/views/FarmsV2/components/FarmTable/Actions/ActionPanel.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import styled, { keyframes, css } from 'styled-components'
-import { LinkExternal } from 'penguinfinance-uikit2'
-import getLiquidityUrlPathParts from 'utils/getLiquidityUrlPathParts'
-import { CommunityTag, CoreTag } from 'components/Tags'
-import { BASE_ADD_LIQUIDITY_URL } from 'config'
-import HarvestAction from './HarvestAction'
-import StakedAction from './StakedAction'
+import { Card, Text, Button, Flex } from 'penguinfinance-uikit2'
+import { ASSET_CONTENT_URL } from 'config'
 import { FarmCardProps } from '../../types';
+import StakePanel from './StakePanel';
 
 const expandAnimation = keyframes`
   from {
@@ -40,99 +37,101 @@ const Container = styled.div<{ expanded }>`
   display: flex;
   width: 100%;
   flex-direction: column-reverse;
-  padding: 24px;
+  padding: 16px 16px 0;
 
   ${({ theme }) => theme.mediaQueries.lg} {
     flex-direction: row;
-    padding: 16px 32px;
+    justify-content: space-between;
+    padding: 16px 16px 0;
   }
 `
 
-const StyledLinkExternal = styled(LinkExternal)`
+const ActionCard = styled(Card)`
+  border-radius: 16px;
+`;
+
+const RewardImage = styled.img<{ size: number, ml?: number }>`
+  height: ${({ size }) => size}px;
+  margin-left: ${({ ml }) => ml && `${ml}px`};
+`;
+
+const StyledButton = styled(Button)`
   font-weight: 400;
-`
+  height: 28px;
+  font-size: 14px;
+`;
 
-const StakeContainer = styled.div`
-  color: ${({ theme }) => theme.colors.text};
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
+const EarningsContainer = styled.div`
+  min-width: 180px;
+`;
 
-  ${({ theme }) => theme.mediaQueries.sm} {
-    justify-content: flex-start;
-  }
-`
-
-const TagsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 25px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-top: 16px;
-  }
-
-  > div {
-    height: 24px;
-    padding: 0 6px;
-    font-size: 14px;
-    margin-right: 4px;
-
-    svg {
-      width: 14px;
-    }
-  }
-`
-
-const ActionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    flex-direction: row;
-    align-items: center;
-    flex-grow: 1;
-    flex-basis: 0;
-  }
-`
-
-const InfoContainer = styled.div`
-  min-width: 200px;
+const Divider = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  height: 2px;
+  margin: 16px auto 8px;
+  width: 100%;
 `
 
 const ActionPanel: React.FunctionComponent<FarmCardProps> = ({
   farm,
   expanded,
 }) => {
-  const isActive = farm.multiplier !== '0X'
-  const liquidityUrlPathParts = getLiquidityUrlPathParts({ quoteTokenAddresses: farm.quoteTokenAddresses, quoteTokenSymbol: farm.quoteTokenSymbol, tokenAddresses: farm.tokenAddresses })
-  const addLiquidityUrl = `${BASE_ADD_LIQUIDITY_URL}/${liquidityUrlPathParts}`
-
-  const chainLink = `https://cchain.explorer.avax.network/address/${
-    farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]
-  }`
-  const info = `https://info.pangolin.exchange/#/pair/${farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]}`
-
   return (
     <Container expanded={expanded}>
-      <InfoContainer>
-        {isActive && (
-          <StakeContainer>
-            <StyledLinkExternal href={addLiquidityUrl}>
-              {`Get ${farm.lpSymbol.toUpperCase()}`}
-            </StyledLinkExternal>
-          </StakeContainer>
-        )}
-        <StyledLinkExternal href={chainLink}>View Contract</StyledLinkExternal>
-        <StyledLinkExternal href={info}>See Pair Info</StyledLinkExternal>
-        <TagsContainer>
-          {farm.isCommunity ? <CommunityTag /> : <CoreTag />}
-        </TagsContainer>
-      </InfoContainer>
-      <ActionContainer>
-        <HarvestAction {...farm} />
-        <StakedAction {...farm} />
-      </ActionContainer>
+      <Flex flexDirection='column' mr='8px' mb='16px'>
+        <ActionCard padding='10px 16px'>
+          <Flex>
+            <Flex flexDirection='column' justifyContent='space-between'>
+              <Text fontSize="20px" color='textSubtle' bold>Your Rewards</Text>
+              <StyledButton color='primary' scale='sm'>Harvest</StyledButton>
+            </Flex>
+            <Flex alignItems='center' ml='40px'>
+              <Flex flexDirection='column'>
+                <RewardImage src='/images/penguin-finance-logo.svg' alt='penguin' size={64} />
+                <Text fontSize='14px' color='textSubtle'>18.24 PEFI</Text>
+              </Flex>
+              <Flex flexDirection='column' ml='32px' mt='4px'>
+                <RewardImage src="/images/pools/iPefi.svg" alt='iPefi' size={56} />
+                <Text mt='4px' fontSize='14px' color='textSubtle'>4.39 iPEFI</Text>
+              </Flex>
+              <Flex flexDirection='column' ml='40px' mt='4px'>
+                <RewardImage src={`${ASSET_CONTENT_URL}/project/pangolin/logo.png`} alt='pangolin' size={56} />
+                <Text mt='4px' fontSize='14px' color='textSubtle'>3.24 PNG</Text>
+              </Flex>
+            </Flex>
+          </Flex>
+        </ActionCard>
+        <ActionCard padding='10px 16px' mt='8px'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text fontSize="20px" color='textSubtle' bold>iPEFI Auto-Nesting</Text>
+            <Text fontSize="12px" color='textDisabled'>Current Allocation: 50%</Text>
+            <StyledButton color='primary' scale='sm'>Modify</StyledButton>
+          </Flex>
+        </ActionCard>
+      </Flex>
+      <ActionCard padding='10px 16px' mr='8px' mb='16px'>
+        <Flex>
+          <EarningsContainer>
+            <Text fontSize="20px" color='textSubtle' bold lineHeight={1} mb='8px'>Your PEFI Earnings</Text>
+            <Text color='textSubtle' fontSize='14px'>Balance: 500 LP</Text>
+            <Text color='textSubtle' fontSize='14px'>Share of Igloo: 15 %</Text>
+          </EarningsContainer>
+          <RewardImage src='/images/farms/pefi-dai.svg' alt='pefi-earning' size={56} />
+        </Flex>
+        <Divider />
+        <Flex> 
+          <EarningsContainer>
+            <Text fontSize="20px" color='textSubtle' bold lineHeight={1} mb='8px'>Igloo Stats</Text>
+            <Text color='textSubtle' fontSize='14px'>PEFI/week: 50,000</Text>
+            <Text color='textSubtle' fontSize='14px'>PEFI/month: 100,000</Text>
+            <Text color='textSubtle' fontSize='14px'>PEFI/year: 200,000</Text>
+          </EarningsContainer>
+          <RewardImage src='/images/farms/pefi-dai.svg' alt='igloo-stats' size={56} />
+        </Flex>
+      </ActionCard>
+      <ActionCard padding='10px 16px' mb='16px'>
+        <StakePanel {...farm} />
+      </ActionCard>
     </Container>
   )
 }
---
+++ src/views/FarmsV2/components/FarmTable/Actions/StakeLPForm.tsx
@@ -0,0 +1,134 @@
+import BigNumber from 'bignumber.js'
+import styled from 'styled-components'
+import React, { useCallback, useMemo, useState } from 'react'
+import { Button, Flex, Text } from 'penguinfinance-uikit2'
+import UnlockButton from 'components/UnlockButton'
+import roundDown from 'utils/roundDown'
+import escapeRegExp from 'utils/escapeRegExp'
+import { PANGOLIN_PEFI_LINK } from 'config'
+import useI18n from 'hooks/useI18n'
+import { getFullDisplayBalance } from 'utils/formatBalance'
+import TokenInput from './TokenInput'
+
+interface DepositModalProps {
+  max: BigNumber
+  onConfirm: (amount: string) => void
+  tokenName?: string
+  account?: string
+  needsApproval: boolean
+  requested: boolean
+  stakingTokenName: string
+  onApprove: () => void
+}
+
+const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`)
+
+const StakeLPForm: React.FC<DepositModalProps> = ({
+  max,
+  onConfirm,
+  tokenName = '',
+  account,
+  needsApproval,
+  requested,
+  stakingTokenName,
+  onApprove,
+}) => {
+  const [val, setVal] = useState('')
+  const [pendingTx, setPendingTx] = useState(false)
+  const TranslateString = useI18n()
+  const fullBalance = useMemo(() => {
+    return getFullDisplayBalance(max)
+  }, [max])
+
+  const handleChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      const nextInput = e.currentTarget.value.replace(/,/g, '.')
+      if (nextInput.length < 25) {
+        if (nextInput === '' || inputRegex.test(escapeRegExp(nextInput))) {
+          setVal(nextInput)
+        }
+      }
+    },
+    [setVal],
+  )
+
+  const handleSelectMax = useCallback(() => {
+    // setVal(roundDown(fullBalance, 2))
+    setVal(fullBalance)
+  }, [fullBalance, setVal])
+
+  const renderText = () => {
+    if (Number(val) > Number(fullBalance) || Number(fullBalance) === 0) return `Get more ${stakingTokenName}`
+    if (pendingTx) return TranslateString(488, 'Pending Confirmation')
+    if (val) return 'Confirm Staking'
+    return 'Enter Amount'
+  }
+
+  const handleStake = async () => {
+    setPendingTx(true)
+    try {
+      await onConfirm(val)
+      setPendingTx(false)
+      setVal('')
+    } catch (error) {
+      setPendingTx(false)
+      setVal('')
+    }
+  }
+
+  const handleGetPefi = () => {
+    window.open(PANGOLIN_PEFI_LINK, '_blank')
+  }
+
+  const canStake = !pendingTx && Number(val) > 0
+
+  return (
+    <>
+      <div>
+        <Text color='textDisabled' fontSize='14px'>LP Token Amount</Text>
+        <TokenInput
+          value={roundDown(val, 2)}
+          onSelectMax={handleSelectMax}
+          onChange={handleChange}
+          max={fullBalance}
+          symbol={tokenName}
+        />
+      </div>
+      <Flex mt="16px">
+        {!account && <StyledUnlockButton />}
+        {account &&
+          (needsApproval ? (
+            <StyledButton disabled={requested} onClick={onApprove} scale="md">
+              {`Approve ${stakingTokenName}`}
+            </StyledButton>
+          ) : (
+            <>
+              {Number(fullBalance) >= Number(val) && Number(fullBalance) > 0 ? (
+                <StyledButton color='red' tokenBalance={val} scale="md" disabled={!canStake} onClick={handleStake}>
+                  {renderText()}
+                </StyledButton>
+              ) : (
+                <StyledButton color='red' tokenBalance={val} scale="md" disabled={pendingTx} onClick={handleGetPefi}>
+                  {renderText()}
+                </StyledButton>
+              )}
+            </>
+          ))}
+      </Flex>
+    </>
+  )
+}
+
+const StyledButton = styled(Button)<{ tokenBalance?: string }>`
+  border-radius: 16px;
+  height: 40px;
+  font-weight: 400;
+`
+
+const StyledUnlockButton = styled(UnlockButton)`
+  border-radius: 16px;
+  height: 40px;
+  font-weight: 400;
+`
+
+export default StakeLPForm
