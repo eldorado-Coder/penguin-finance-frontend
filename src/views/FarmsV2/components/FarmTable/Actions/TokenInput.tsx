ddff4c1fcce917ed4e1046f077876c77b9fae790
evercreativedev <evercreativedev@gmail.com>
2021-09-04 02:45:43 +0800

    feat: updated igloo v2 action panel

--- src/views/FarmsV2/components/FarmTable/Actions/TokenInput.tsx
+++ src/views/FarmsV2/components/FarmTable/Actions/TokenInput.tsx
@@ -11,38 +11,33 @@ interface TokenInputProps extends InputProps {
   onSelectMax?: () => void
 }
 
-const TokenInput: React.FC<TokenInputProps> = ({ value, onChange, onSelectMax }) => {
+const TokenInput: React.FC<TokenInputProps> = ({ value, symbol, onChange, onSelectMax }) => {
   const TranslateString = useI18n()
   return (
     <StyledTokenInput>
-      <Input className="input" inputClassName="tokenInput" onChange={onChange} placeholder="" value={value} />
-      <Wrapper>
-        <TokenValueWrapper justifyContent="space-between" alignItems="center">
-          <TokenValue>{`${value || 0} LP`}</TokenValue>
-          <StyledButton scale="sm" onClick={onSelectMax}>
-            {TranslateString(452, 'Max')}
-          </StyledButton>
-        </TokenValueWrapper>
-      </Wrapper>
+      <Input 
+        className="input" 
+        inputClassName="tokenInput" 
+        onChange={onChange} 
+        placeholder="" 
+        value={value}
+        endAdornment={
+          <Flex alignItems='center'>
+            <StyledTokenSymbol>{symbol}</StyledTokenSymbol>
+            <StyledButton scale="sm" onClick={onSelectMax}>
+              {TranslateString(452, 'Max')}
+            </StyledButton>
+          </Flex>
+        } />
     </StyledTokenInput>
   )
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 0;
-  position: relative;
-  bottom: 2.5rem;
-`
-
-const TokenValueWrapper = styled(Flex)`
-  height: 2.5rem;
-  padding: 0 16px;
-`
-
-const TokenValue = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
+const StyledTokenSymbol = styled.span`
+  color: ${({ theme }) => theme.isDark ? theme.colors.primary : '#372871'};
+  font-weight: 500;
+  margin: 0 8px;
+  font-size: 14px;
 `
 
 const StyledTokenInput = styled.div`
@@ -50,27 +45,14 @@ const StyledTokenInput = styled.div`
     height: 2.5rem;
     border-radius: 8px;
     background: ${({ theme }) => (theme.isDark ? '#604E84' : '#ECE8F2')};
-    font-size: 16px;
+    font-size: 14px;
+
+    caret-color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
+    color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
 
     .tokenInput {
-      caret-color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
-      color: transparent;
-      z-index: 1;
-      margin-right: 80px;
-      height: 2.5rem;
-      font-size: 16px;
-
-      ::placeholder {
-        color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
-      }
-
-      :-ms-input-placeholder {
-        color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
-      }
-
-      ::-ms-input-placeholder {
-        color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
-      }
+      color: ${({ theme }) => (theme.isDark ? 'white' : '#372871')};
+      font-size: 14px;
     }
   }
 `
@@ -79,7 +61,8 @@ const StyledButton = styled(Button)`
   height: 28px;
   font-size: 14px;
   font-weight: 400;
-  color: ${({ theme }) => theme.isDark && '#30264f'};
+  background-color: ${({ theme }) => theme.colors.red};
+  color: white;
 `
 
 export default TokenInput
