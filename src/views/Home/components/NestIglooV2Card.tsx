7bee76eb53215a23558e5c082dfae860d903e1b2
daiki saito <daiki.saito92@gmail.com>
2021-09-10 23:16:38 +0900

    implement "turbo-igloo" card

--- src/views/Home/components/NestIglooV2Card.tsx
+++ src/views/Home/components/NestIglooV2Card.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Card, useMatchBreakpoints } from 'penguinfinance-uikit2'
+import { useHistory } from 'react-router-dom'
 
 const StyledNestIglooCard = styled(Card)<{ isMobile?: boolean }>`
-  height: ${({ isMobile }) => isMobile ? 'calc(50vw - 16px)' : '230px'};
+  height: ${({ isMobile }) => (isMobile ? 'calc(50vw - 16px)' : '230px')};
   margin-bottom: 24px;
   background: ${({ theme }) => theme.isDark && '#30264F'};
 `
@@ -12,16 +13,16 @@ const StyledNestIglooCardBg = styled.div`
   height: 100%;
   background-size: cover;
   background-position: center center;
-  background-image: url('/images/home/ipefi-introducing.png');
+  background-image: url('/images/home/turbo_igloo.png');
   cursor: pointer;
 `
 
 const NestIglooV2Card = () => {
+  const history = useHistory()
   const { isXl } = useMatchBreakpoints()
   const isMobile = !isXl
   const onClickNestIglooV2Card = () => {
-    const iglooV2Url = 'https://penguin-finance.medium.com/introducing-ipefi-the-nest-evolution-d002f8548276'
-    window.open(iglooV2Url, '_blank')
+    history.push('/farms')
   }
 
   return (
