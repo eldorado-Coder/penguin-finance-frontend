149ac0316eb594865a5af4b6c34db2e5b387894b
daiki saito <daiki.saito92@gmail.com>
2021-09-22 00:52:10 +0900

    update arena page

--- src/views/Arena/Arena.tsx
+++ src/views/Arena/Arena.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import Sound from 'react-sound'
 import styled from 'styled-components'
 import Page from 'components/layout/Page'
-import useUserSetting from 'hooks/useUserSetting'
 
 const arenaBgVideoUrl = 'https://res.cloudinary.com/dbyunrpzq/video/upload/v1624544901/penguin_arena_clpyb0.mp4'
 
 const Arena: React.FC = () => {
-  const { isMusic } = useUserSetting()
-  const volumeSize = isMusic ? 100 : 0
-
   return (
     <Page>
-      <Sound url="/sounds/penguin_arena_page.mp3" playStatus={Sound.status.PLAYING} loop volume={volumeSize} />
-      <ArenaBgContainer width="100%" height="100%" autoPlay loop muted>
+      <ArenaBgContainer width="100%" height="100%" autoPlay loop controls>
         <source src={arenaBgVideoUrl} />
       </ArenaBgContainer>
     </Page>
