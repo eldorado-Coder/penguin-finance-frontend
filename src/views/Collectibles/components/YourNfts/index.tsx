be82c2c741fbe4f1a28ce33ed48572dc0d005cee
daiki saito <daiki.saito92@gmail.com>
2021-10-05 12:22:49 +0900

    change name of "YourNfts" component

--- src/views/Collectibles/components/YourNfts/index.tsx
+++ src/views/Collectibles/components/YourNfts/index.tsx
@@ -16,39 +16,41 @@ const CardGrid = styled(NftGrid)`
   }
 `
 
-const NftList = () => {
+const YourNfts = () => {
   const { account } = useWeb3React()
-  const userCollectibles = useUserCollectibles(account);
-  
+  const userCollectibles = useUserCollectibles(account)
+
   const nftCollections = useMemo(() => {
-    const collections = [];
-    const userNfts = nfts.filter(nft => userCollectibles.nftIds.find(nftId => nftId === nft.bunnyId));
+    const collections = []
+    const userNfts = nfts.filter((nft) => userCollectibles.nftIds.find((nftId) => nftId === nft.bunnyId))
     // eslint-disable-next-line no-restricted-syntax
     for (const nft of userNfts) {
-      const collectionIndex = collections.findIndex(collection => collection.name === nft.collection);
+      const collectionIndex = collections.findIndex((collection) => collection.name === nft.collection)
 
       if (collectionIndex > -1) {
-        collections[collectionIndex].nftList.push(nft);
+        collections[collectionIndex].nftList.push(nft)
       } else {
         collections.push({
           name: nft.collection,
-          nftList: [nft]
-        });
+          nftList: [nft],
+        })
       }
     }
 
-    return collections;
-  }, [userCollectibles]);
+    return collections
+  }, [userCollectibles])
 
   return (
     <>
-      {account && nftCollections.length > 0 &&
+      {account && nftCollections.length > 0 && (
         <>
           <CardGrid>
-            <Text bold fontSize='24px' color='primary'>Your Collectible NFTs</Text>
+            <Text bold fontSize="24px" color="primary">
+              Your Collectible NFTs
+            </Text>
           </CardGrid>
-          {nftCollections.map(nftCollection => {
-            return (nftCollection.nftList.length > 0 ?
+          {nftCollections.map((nftCollection) => {
+            return nftCollection.nftList.length > 0 ? (
               <CardGrid key={`your-nft-${nftCollection.name}`}>
                 {orderBy(nftCollection.nftList, 'sortOrder').map((nft) => {
                   return (
@@ -58,13 +60,14 @@ const NftList = () => {
                   )
                 })}
               </CardGrid>
-              : <div key={nftCollection.collectionName} />
+            ) : (
+              <div key={nftCollection.collectionName} />
             )
           })}
         </>
-      }
+      )}
     </>
   )
 }
 
-export default NftList
+export default YourNfts
