ef2620cbd58ed993bb22af2dbaa442c026bc4c69
daiki saito <daiki.saito92@gmail.com>
2021-10-08 18:18:38 +0900

    change artwork card on launchpad page for mobile

--- src/views/LaunchpadBoofi/ArtworkCard/index.tsx
+++ src/views/LaunchpadBoofi/ArtworkCard/index.tsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useMatchBreakpoints } from 'penguinfinance-uikit2'
 import { useHistory } from 'react-router-dom'
 import Card from '../Card'
 
 const ArtworkCard = () => {
   const history = useHistory()
+  const { isXl } = useMatchBreakpoints()
+  const isMobile = !isXl
 
   const onClickArtworkCard = () => {
     history.push('/club')
   }
 
   return (
-    <StyledCard>
+    <StyledCard isMobile={isMobile}>
       <StyledCardBg onClick={onClickArtworkCard} />
     </StyledCard>
   )
 }
 
-const StyledCard = styled(Card)`
+const StyledCard = styled(Card)<{ isMobile?: boolean }>`
   border-radius: 8px;
   margin-top: 16px;
   width: 100%;
   box-shadow: 0px 1px 6px rgb(0 0 0 / 16%);
+  height: ${({ isMobile }) => isMobile && 'calc(50vw - 16px)'};
 
   ${({ theme }) => theme.mediaQueries.lg} {
     width: 49%;
