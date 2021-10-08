7ea74410be1c8e8bbcf93b852832887982ad4cd8
daiki saito <daiki.saito92@gmail.com>
2021-10-08 11:26:29 +0900

    remove "live" label from launchpad menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -44,12 +44,13 @@ const Menu = (props) => {
   //   ...links[collectiblesIndex],
   //   badge: isCollectiblesNew ? 'NEW' : '',
   // }
-  links[launchpadIndex] = {
-    ...links[launchpadIndex],
-    badge: 'LIVE',
-    badgeLightColor: '#37DB94',
-    badgeDarkColor: '#37DB94',
-  }
+
+  // links[launchpadIndex] = {
+  //   ...links[launchpadIndex],
+  //   badge: 'LIVE',
+  //   badgeLightColor: '#37DB94',
+  //   badgeDarkColor: '#37DB94',
+  // }
 
   // links[emperorIndex] = {
   //   ...links[emperorIndex],
