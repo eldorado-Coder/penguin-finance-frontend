0afce23219eb76223b0527a1536e72db2a5baca2
daiki saito <daiki.saito92@gmail.com>
2021-10-14 21:36:24 +0900

    change artwork card

--- src/views/HomeV2/components/ArtworkCard.tsx
+++ src/views/HomeV2/components/ArtworkCard.tsx
@@ -3,6 +3,25 @@ import styled from 'styled-components'
 import { Card, useMatchBreakpoints } from 'penguinfinance-uikit2'
 import { useHistory } from 'react-router-dom'
 
+const ArtworkCard = () => {
+  const history = useHistory()
+  const { isXl } = useMatchBreakpoints()
+  const isMobile = !isXl
+  const onClickArtworkV2Card = () => {
+    history.push('/club')
+    // window.open(
+    //   'https://penguin-finance.medium.com/penguin-launchpad-boofinance-ido-tiers-are-here-29ba4cd90053',
+    //   '_blank',
+    // )
+  }
+
+  return (
+    <StyledArtworkCard isMobile={isMobile}>
+      <StyledArtworkCardBg onClick={onClickArtworkV2Card} />
+    </StyledArtworkCard>
+  )
+}
+
 const StyledArtworkCard = styled(Card)<{ isMobile?: boolean }>`
   height: ${({ isMobile }) => (isMobile ? 'calc(50vw - 16px)' : '380px')};
   /* margin-bottom: 24px; */
@@ -13,27 +32,8 @@ const StyledArtworkCardBg = styled.div`
   height: 100%;
   background-size: cover;
   background-position: center center;
-  background-image: url('/images/home/boofinance-tiersartwork.png');
+  background-image: url('/images/launchpad/club_penguin_live.png');
   cursor: pointer;
 `
-
-const ArtworkCard = () => {
-  const history = useHistory()
-  const { isXl } = useMatchBreakpoints()
-  const isMobile = !isXl
-  const onClickArtworkV2Card = () => {
-    // history.push('/farms')
-    window.open(
-      'https://penguin-finance.medium.com/penguin-launchpad-boofinance-ido-tiers-are-here-29ba4cd90053',
-      '_blank',
-    )
-  }
-
-  return (
-    <StyledArtworkCard isMobile={isMobile}>
-      <StyledArtworkCardBg onClick={onClickArtworkV2Card} />
-    </StyledArtworkCard>
-  )
-}
 
 export default ArtworkCard
