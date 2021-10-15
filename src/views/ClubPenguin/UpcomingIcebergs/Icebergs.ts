b78ab091ea12aef89b69a6e9f2b93cf48642ac1c
daiki saito <daiki.saito92@gmail.com>
2021-10-15 23:18:47 +0900

    update upcoming icebergs

--- src/views/ClubPenguin/UpcomingIcebergs/Icebergs.ts
+++ src/views/ClubPenguin/UpcomingIcebergs/Icebergs.ts
@@ -1,15 +1,15 @@
 export const Icebergs = [
-  {
-    title: 'VSO ICEBERG',
-    // tag: 'Marketplace',
-    tag: 'DeFi',
-    // toDistribute: '$20,000',
-    toDistribute: 'TBD',
-    earn: 'VSO',
-    logo: 'verso_iceberg.svg',
-    description: 'A decentralized marketplace for financial products and DeFi mass adoption via regulated e-wallets',
-    url: 'https://verso.finance',
-  },
+  // {
+  //   title: 'VSO ICEBERG',
+  //   // tag: 'Marketplace',
+  //   tag: 'DeFi',
+  //   // toDistribute: '$20,000',
+  //   toDistribute: 'TBD',
+  //   earn: 'VSO',
+  //   logo: 'verso_iceberg.svg',
+  //   description: 'A decentralized marketplace for financial products and DeFi mass adoption via regulated e-wallets',
+  //   url: 'https://verso.finance',
+  // },
   {
     title: 'BOOFI ICEBERG',
     tag: 'Yield Farming',
@@ -20,6 +20,16 @@ export const Icebergs = [
     description:
       'A new Avalanche project developing a unique yield farming dApp, deflationary NFTs, and an all-encompassing DAO.',
     url: 'https://boofinance.gitbook.io/boo-finance/',
+  },
+  {
+    title: 'PNG ICEBERG',
+    tag: 'Exchange',
+    // toDistribute: '43,000 BOOFI',
+    toDistribute: 'TBD',
+    earn: 'PNG',
+    logo: 'boofi_iceberg.svg',
+    description: `A community-driven decentralized exchange for Avalanche. They've truly helped better the Avalanche Ecosystem by helping new native projects.`,
+    url: 'https://docs.pangolin.exchange/',
   },
   {
     title: 'AVAX ICEBERG',
