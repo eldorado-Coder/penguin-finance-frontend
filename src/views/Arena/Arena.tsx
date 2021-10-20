78bd16dec636f70f3d1c3f53c33c728e6e9b95bb
daiki saito <daiki.saito92@gmail.com>
2021-10-20 21:29:56 +0900

    fix footer issue on arena page

--- src/views/Arena/Arena.tsx
+++ src/views/Arena/Arena.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 import Page from 'components/layout/Page'
 
-const arenaBgVideoUrl = 'https://res.cloudinary.com/dbyunrpzq/video/upload/v1632206011/The_Journey_to_the_Arena_Part_01_uvqzti.mp4'
+const arenaBgVideoUrl =
+  'https://res.cloudinary.com/dbyunrpzq/video/upload/v1632206011/The_Journey_to_the_Arena_Part_01_uvqzti.mp4'
 
 const Arena: React.FC = () => {
   return (
-    <Page>
+    <ArenaPage>
       <ArenaBgContainer width="100%" height="100%" autoPlay loop controls>
         <source src={arenaBgVideoUrl} />
       </ArenaBgContainer>
-    </Page>
+    </ArenaPage>
   )
 }
+
+const ArenaPage = styled(Page)`
+  height: 100%;
+`
 
 const ArenaBgContainer = styled.video`
   object-fit: fill;
