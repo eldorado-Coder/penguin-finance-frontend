96c142845edb56360ac10cc3c16fdec96ea3ef6e
Steve <steve.yerigan.dev@gmail.com>
2021-11-05 09:48:22 -0500

    Added an empty line

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from 'react'
 const useMediaQuery = (query: string) => {
   const mediaQuery = window.matchMedia(query)
   const [match, setMatch] = useState(!!mediaQuery.matches)
+
   useEffect(() => {
     const handler = () => setMatch(!!mediaQuery.matches)
     mediaQuery.addEventListener('change', handler)
