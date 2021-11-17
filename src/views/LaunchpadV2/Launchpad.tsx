d7d3bdaa6b091a613643d0f4f4945b5a66cf0281
daiki saito <daiki.saito92@gmail.com>
2021-11-17 12:13:24 +0900

    update light/dark banners of launchpad-v2 page

--- src/views/LaunchpadV2/Launchpad.tsx
+++ src/views/LaunchpadV2/Launchpad.tsx
@@ -1,60 +1,71 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Text, Flex } from 'penguinfinance-uikit2'
+import useTheme from 'hooks/useTheme'
 import Page from 'components/layout/Page'
-import UpcomingIDOs from './components/UpcomingIDOs';
-import CompletedIDOs from './components/CompletedIDOs';
-import AcoomalatingSteps from './components/AcoomalatingSteps';
+import UpcomingIDOs from './components/UpcomingIDOs'
+import CompletedIDOs from './components/CompletedIDOs'
+import AcoomalatingSteps from './components/AcoomalatingSteps'
 
 const TOP_TIER_PROJECTS = [
   {
     imgUrl: `${process.env.PUBLIC_URL}/images/launchpad-v2/top-tier-projects/apply-project.png`,
     name: 'apply-project',
-    link: 'https://docs.google.com/forms/u/2/d/e/1FAIpQLSdgM5oq-3shkNQeEhTGHP-JDiRY6Y3URaGnKCf6QVt4qSJVMA/viewform'
+    link: 'https://docs.google.com/forms/u/2/d/e/1FAIpQLSdgM5oq-3shkNQeEhTGHP-JDiRY6Y3URaGnKCf6QVt4qSJVMA/viewform',
   },
   {
     imgUrl: `${process.env.PUBLIC_URL}/images/launchpad-v2/top-tier-projects/trader-joe.png`,
     name: 'trader-joe',
-    link: 'https://traderjoexyz.com/#/trade?outputCurrency=0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c'
+    link: 'https://traderjoexyz.com/#/trade?outputCurrency=0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c',
   },
   {
     imgUrl: `${process.env.PUBLIC_URL}/images/launchpad-v2/top-tier-projects/apply-project.png`,
     name: 'pangolin',
-    link: 'https://app.pangolin.exchange/#/swap?outputCurrency=0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c'
+    link: 'https://app.pangolin.exchange/#/swap?outputCurrency=0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c',
   },
   {
     imgUrl: `${process.env.PUBLIC_URL}/images/launchpad-v2/top-tier-projects/lydia.png`,
     name: 'lydia',
-    link: 'https://exchange.lydia.finance/#/swap?inputCurrency=0xe896cdeaac9615145c0ca09c8cd5c25bced6384c'
-  }
+    link: 'https://exchange.lydia.finance/#/swap?inputCurrency=0xe896cdeaac9615145c0ca09c8cd5c25bced6384c',
+  },
 ]
 const Launchpad: React.FC = () => {
-  const handleLinkProject = projectLink => () => {
-    window.open(projectLink, '_blank');
-  };
+  const theme = useTheme()
+
+  const handleLinkProject = (projectLink) => () => {
+    window.open(projectLink, '_blank')
+  }
 
   const handleViewTelegramAnnouncements = () => {
-    window.open('https://t.me/pefi_announcements', '_blank');
-  };
+    window.open('https://t.me/pefi_announcements', '_blank')
+  }
 
   return (
     <div>
-      <IgloosBannerContainer justifyContent='center'>
+      <IgloosBannerContainer justifyContent="center">
         <BannerImage
-          src={`${process.env.PUBLIC_URL}/images/launchpad-v2/banner.png`}
+          src={
+            theme.isDark
+              ? `${process.env.PUBLIC_URL}/images/launchpad-v2/banners/dark_banner.png`
+              : `${process.env.PUBLIC_URL}/images/launchpad-v2/banners/light_banner.png`
+          }
           alt="launchpad banner"
         />
         <HeaderContainer>
-          <HeaderTitle fontSize='48px' fontWeight={800} color='white' mb='8px'>THE FIRST & FAIREST LAUNCHPAD PROTOCOL ON THE AVALANCHE NETWORK</HeaderTitle>
-          <Description fontSize='24px' lineHeight='31px' color='white'>Stake iPEFI and earn allocations to invest in top-tier projects.</Description>
-          <Description fontSize='24px' lineHeight='31px' color='white'>No KYC required.</Description>
-          <TopTierProjects mt='32px'>
-            {TOP_TIER_PROJECTS.map(project => {
+          <HeaderTitle fontSize="48px" fontWeight={800} color="white" mb="8px">
+            THE FIRST & FAIREST LAUNCHPAD PROTOCOL ON THE AVALANCHE NETWORK
+          </HeaderTitle>
+          <Description fontSize="24px" lineHeight="31px" color="white">
+            Stake iPEFI and earn allocations to invest in top-tier projects.
+          </Description>
+          <Description fontSize="24px" lineHeight="31px" color="white">
+            No KYC required.
+          </Description>
+          <TopTierProjects mt="32px">
+            {TOP_TIER_PROJECTS.map((project) => {
               return (
                 <Text key={project.name} onClick={handleLinkProject(project.link)}>
-                  <img 
-                    src={project.imgUrl} 
-                    alt={project.name} />
+                  <img src={project.imgUrl} alt={project.name} />
                 </Text>
               )
             })}
@@ -66,11 +77,15 @@ const Launchpad: React.FC = () => {
         <AcoomalatingSteps />
         <UpcomingIDOs />
         <CompletedIDOs />
-        <Flex mt='80px' mb='80px' justifyContent='center'>
-          <AnnouncementText color='white' fontSize='25px'>
-            Subscribe to our <ViewChannel fontSize='25px' color='white' onClick={handleViewTelegramAnnouncements}>Telegram announcements channel</ViewChannel> to learn about new project launches as soon as possible.
+        <Flex mt="80px" mb="80px" justifyContent="center">
+          <AnnouncementText color="white" fontSize="25px">
+            Subscribe to our{' '}
+            <ViewChannel fontSize="25px" color="white" onClick={handleViewTelegramAnnouncements}>
+              Telegram announcements channel
+            </ViewChannel>{' '}
+            to learn about new project launches as soon as possible.
           </AnnouncementText>
-          <TelegramImage src={`${process.env.PUBLIC_URL}/images/launchpad-v2/telegram.png`} alt='telegram' />
+          <TelegramImage src={`${process.env.PUBLIC_URL}/images/launchpad-v2/telegram.png`} alt="telegram" />
         </Flex>
       </LaunchpadPage>
     </div>
@@ -82,7 +97,7 @@ const LaunchpadPage = styled(Page)`
 `
 
 const LaunchpadBgContainer = styled.div`
-  background-image: url(/images/launchpad-v2/background.svg);
+  background-image: url('/images/launchpad-v2/background.svg');
   background-repeat: repeat;
   background-size: contain;
   position: absolute;
@@ -91,7 +106,7 @@ const LaunchpadBgContainer = styled.div`
   right: 0px;
   left: 0px;
   z-index: -1;
-  background-color: ${({ theme }) => theme.isDark ? '#2e2152' : '#483692'};
+  background-color: ${({ theme }) => (theme.isDark ? '#2e2152' : '#483692')};
 `
 
 // banner
@@ -138,7 +153,7 @@ const Description = styled(Text)`
     line-height: 36px;
     text-align: left;
   }
-`;
+`
 
 const HeaderTitle = styled(Text)`
   font-size: 24px;
@@ -151,7 +166,7 @@ const HeaderTitle = styled(Text)`
     line-height: 72px;
     text-align: left;
   }
-`;
+`
 
 const HeaderContainer = styled.div`
   position: absolute;
@@ -171,12 +186,12 @@ const HeaderContainer = styled.div`
     min-width: 1236px;
     margin: 100px 64px 0;
   }
-`;
+`
 
 const TopTierProjects = styled(Flex)`
   flex-wrap: wrap;
   justify-content: center;
-  
+
   div {
     margin-right: 16px;
     margin-bottom: 16px;
@@ -202,23 +217,23 @@ const TopTierProjects = styled(Flex)`
   @media (min-width: 968px) {
     justify-content: flex-start;
   }
-`;
+`
 
 const AnnouncementText = styled(Text)`
   max-width: 750px;
   text-align: center;
-`;
+`
 
 const ViewChannel = styled(Text)`
   text-decoration: underline;
   cursor: pointer;
   display: inline;
-`;
+`
 
 const TelegramImage = styled.img`
   width: 48px;
   height: 48px;
   margin-left: 8px;
-`;
+`
 
 export default Launchpad
