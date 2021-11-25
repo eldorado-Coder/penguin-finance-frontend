87db48a2e704bb2ca043ccbb8b63ecf0856d5b8e
daiki saito <daiki.saito92@gmail.com>
2021-11-25 19:15:54 +0900

    update "GetStart" and "DiscoverArena" cards

---
+++ src/views/HomeV2/components/GetSmartCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card } from 'penguinfinance-uikit2'
+
+const GetSmartCard = () => {
+  const onClickCard = () => {
+    window.open('https://docs.penguinfinance.io/', '_blank')
+  }
+
+  return <StyledCard onClick={onClickCard} />
+}
+
+const StyledCard = styled(Card)`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  width: 100%;
+  background-size: cover;
+  background-position: left center;
+  background-image: url('/images/home-v2/get_smart.png');
+  border-radius: 26px;
+  box-shadow: 0px 1px 8px rgb(0 0 0 / 24%);
+  min-height: 84px;
+  ${({ theme }) => theme.mediaQueries.lg} {
+    margin: 0;
+    max-width: none;
+  }
+  ${({ theme }) => theme.mediaQueries.sm} {
+    min-height: 157px;
+  }
+`
+
+export default GetSmartCard
